5bf7305c5b2e043b6df34b99bef5deb41eb8cc40
citruslimes09 lim <citruslimes09@gmail.com>
2023-11-14 06:47:21 +0000

    qassa

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
-if [ -f ~/rom/out/target/product/citrus/Havoc-OS-*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/citrus/ && ls Havoc-OS-*.zip)"
-      rclone copy ~/rom/out/target/product/citrus/Havoc-OS-*.zip 298:citrus -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/citrus/ && ls Havoc-OS-*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/juice/qassa-*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/juice/ && ls qassa-*.zip)"
+      rclone copy ~/rom/out/target/product/juice/qassa-*.zip 298:juice -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/juice/ && ls qassa-*.zip) Uploaded Successfully!"
 fi
